a8769e9d01454e96dca43c8fbaed05efd5f4d452
jhlee1108 <jeonghyeon.lee1108@gmail.com>
2016-09-20 10:47:34 +0900

    Fix merge_sort.c

--- Algorithm/merge_sort.c
+++ Algorithm/merge_sort.c
@@ -1,64 +1,66 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
-#define MAX_SIZE 100000
+#define MAX_SIZE 1000000
 
-void merge(int *a, int start, int mid, int end)
+void merge(int *a, int n)
 {
-	int *temp = (int*)malloc(sizeof(int) * MAX_SIZE);
-	int i;
-	int s = start;
-	int m = mid;
-	int t = start;
+	int *temp = (int*)malloc(sizeof(int) * n);
+	int mid = n / 2;
+	int i = 0;
+	int j = mid;
+	int t = 0;
+	int k;
 
-	for(i = start; i < end; i++)
+	for(k = 0; k < n; k++)
 	{
-		temp[i] = a[i];
+		temp[k] = a[k];
 	}
 
-	while((s < mid) && (m < end))
+	while((i < mid) && (j < n))
 	{
-		if(temp[s] < temp[m])
+		if(temp[i] < temp[j])
 		{
-			a[t++] = temp[s++];
+			a[t++] = temp[i++];
 		}
 		else
 		{
-			a[t++] = temp[m++];
+			a[t++] = temp[j++];
 		}
 	}
 
-	if(s == m)
+	if(i == mid)
 	{
-		for(i = m; i < end; i++)
+		for(k = j; k < n; k++)
 		{
-			a[t++] = temp[i];
+			a[t++] = temp[k];
 		}
 	}
 
-	if(m == end)
+	else
 	{
-		for(i = s; i < mid; i++)
+		for(k = i; k < mid; k++)
 		{
-			a[t++] = temp[i];
+			a[t++] = temp[k];
 		}
 	}
 
 	free(temp);
 }
 
-void merge_sort(int *a, int start, int end)
+void merge_sort(int *a, int n)
 {
-	int mid = (start + end) / 2;
+	int mid = n / 2;
 
-	if((end - start) <= 1)
+	if(n <= 1)
 	{
 		return;
 	}
 
-	merge_sort(a,start,mid);
-	merge_sort(a,mid,end);
-	merge(a,start,mid,end);
+	merge_sort(a,mid);
+	merge_sort(a + mid,n - mid);
+	merge(a,n);
 }
 
 void write_array(int *a, int n)
@@ -78,18 +80,28 @@ void write_array(int *a, int n)
 
 int main()
 {
-	FILE *fp = fopen("hw02_10man.txt","r");
+	FILE *fp = fopen("hw02_100man.txt","r");
 	int *a = (int*)malloc(sizeof(int) * MAX_SIZE);
 	int n = 0;
+	time_t start_time = 0, end_time = 0;
 
+	printf("Reading input file...\n");
 	while(0 < fscanf(fp,"%d,",&a[n]))
 	{
 		n++;
 	}
 
-	merge_sort(a,0,n);
+	printf("Start merge sort\n");
+	start_time = clock();
+	merge_sort(a,n);
+	end_time = clock();
+	printf("Finish\n");
+	printf("%d ms\n", end_time - start_time);
+
+	printf("Writing array...\n");
 	write_array(a,n);
 	fclose(fp);
+
 
 	return 0;
 }
